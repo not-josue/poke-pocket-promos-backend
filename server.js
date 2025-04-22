88198d5d17ab81c45452c44fbeb90c05d83fa899
not-josue <129870578+not-josue@users.noreply.github.com>
2025-04-22 11:50:40 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require("express");
 const cors = require("cors");
 const joi = require("joi");
 const app = express();
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
+
+// MongoDB Connection
+mongoose
+  .connect("mongodb+srv://admin:g73qPnmIfXBabAuR@cluster0.ieszscz.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB Atlas...");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
+
+// Schema for Comments
+// Comment id is generated automatically by MongoDB
+const commentSchema = new mongoose.Schema({
+  cardid: Number,
+  name: String,
+  text: String,
+  timestamp: Date,
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -30,7 +52,7 @@ app.get("/releases", (req, res) => {
 });
 
 // Comments
-let comments = [];
+// let comments = [];
 
 const schema = joi.object({
   cardid: joi.number().required(),
@@ -38,62 +60,94 @@ const schema = joi.object({
   text: joi.string().min(5).max(200).required(),
 });
 
-app.post("/comments", (req, res) => {
+app.post("/comments", async (req, res) => {
   const { error } = schema.validate(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const comment = {
-    id: comments.length + 1,
+  const comment = new Comment({
+    // id: comments.length + 1,
     cardid: Number(req.body.cardid),
+    name: req.body.name,
+    text: req.body.text,
+    timestamp: new Date(),
+  });
+
+  // comments.push(comment);
+  const newComment = await comment.save();
+  res.status(201).send(newComment);
+});
+
+app.get("/comments", async (req, res) => {
+  // const comments = await Comment.find();
+  // console.log(comments);
+  // res.send(comments);
+
+  const { cardid } = req.query;
+
+  try {
+    // replaces old filtering GET request
+    // works for url?cardid=n ; where n is the cardid
+    if (cardid !== undefined) {
+      const filtered = await Comment.find({ cardid: Number(cardid) });
+      return res.send(filtered);
+    }
+    // All comments if no query search
+    const allComments = await Comment.find();
+    res.send(allComments);
+  } catch (err) {
+    console.error("Error fetching comments:", err);
+    res.status(500).send("Server error while fetching comments");
+  }
+});
+
+// Editing Comments
+app.put("/comments/:id", async (req, res) => {
+  // const { id, name, text } = req.body;
+  // const index = comments.findIndex((el) => el.id === Number(id));
+
+  // if (index === -1) return res.status(404).send("Comment not found...");
+
+  // comments[index].name = name;
+  // comments[index].text = text;
+  // comments[index].timestamp = new Date();
+
+  const updateSchema = joi.object({
+    name: joi.string().min(2).max(20),
+    text: joi.string().min(5).max(200),
+  });
+
+  const { error } = updateSchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const fieldsToUpdate = {
     name: req.body.name,
     text: req.body.text,
     timestamp: new Date(),
   };
 
-  comments.push(comment);
-  res.status(201).send(comment);
-});
+  const result = await Comment.findByIdAndUpdate(
+    req.params.id,
+    fieldsToUpdate,
+    { new: true } // Returns the updated document instead of the original
+  );
 
-app.get("/comments", (req, res) => {
-  res.send(comments);
-});
-
-app.get("/comments/:id", (req, res) => {
-  const { cardid } = req.query;
-
-  if (!cardid) return res.status(400).send("No comments for this card...");
-
-  const filter = comments.filter((el) => el.cardid === Number(cardid));
-
-  res.send(filter);
-});
-
-// Editing Comments
-app.put("/comments", (req, res) => {
-  const { id, name, text } = req.body;
-  const index = comments.findIndex((el) => el.id === Number(id));
-
-  if (index === -1) return res.status(404).send("Comment not found...");
-
-  comments[index].name = name;
-  comments[index].text = text;
-  comments[index].timestamp = new Date();
-
-  res.send(comments[index]);
+  res.send(result);
 });
 
 // Deleting Comments
-app.delete("/comments/:id", (req, res) => {
-  const commentId = Number(req.params.id);
-  const index = comments.findIndex((el) => el.id === commentId);
+app.delete("/comments/:id", async (req, res) => {
+  // const commentId = Number(req.params.id);
+  // const index = comments.findIndex((el) => el.id === commentId);
 
-  if (index === -1) return res.status(404).send("Comment not found...");
+  // if (index === -1) return res.status(404).send("Comment not found...");
 
-  const deletedComment = comments[index];
-  comments.splice(index, 1);
+  // const deletedComment = comments[index];
+  // comments.splice(index, 1);
 
-  res.send(deletedComment);
+  const comment = await Comment.findByIdAndDelete(req.params.id);
+
+  res.send(comment);
 });
 
 // Start Server
